46de16a2f2ff43775c7d96a30fe5859334329b16
YeXiuPH <kirivps@gmail.com>
2023-07-12 22:37:37 +0800

    Add project files.

---
+++ RANCore/RANCore.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "RANCore.h"
+
+RANCore::RANCore() {
+
+}
+
+RANCore::~RANCore() {
+
+}
+
+void RANCore::extractRCC() {
+}
---
+++ RANCore/RANCore.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "pch.h"
+#include "framework.h"
+
+class RANCore {
+
+
+public:
+	RANCore();
+	~RANCore();
+	void extractRCC();
+};
